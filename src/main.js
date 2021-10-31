ae6a1628cf546df7296a22c8299a168eaa6c7fa8
tixwho <905002146tix@gmail.com>
2021-10-31 09:55:23 -0500

    dependency update 3& antd intro

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import Button from 'ant-design-vue/lib/button';
+import { createApp } from 'vue';
+import App from './App';
+import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+import router from './router'
+import store from './store'
 
-var app = createApp(App);
-app.component(Button.name,Button);
-
-app.mount('#app')
+const app = createApp(App).use(store).use(router);
+app.use(Antd);
+app.mount("#app");

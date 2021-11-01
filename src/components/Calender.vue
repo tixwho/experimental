1e359979de2e0ef16ecb45ecfab2c65a6e81418b
tixwho <905002146tix@gmail.com>
2021-11-01 18:50:56 -0500

    calendar update

--- src/components/Calender.vue
+++ src/components/Calender.vue
@@ -1,22 +1,114 @@
 <template>
-    <div class='cal-container'>
-        <a-calendar class='center-big' v-model:value="value" :fullscreen="true" @panelChange="onPanelChange" />
-  </div>
+  <a-calendar v-model:value="value">
+    <template #dateCellRender="{ current }">
+      <ul class="events">
+        <li v-for="item in getListData(current)" :key="item.content">
+          <a-badge :status="item.type" :text="item.content" />
+        </li>
+      </ul>
+    </template>
+    <template #monthCellRender="{ current }">
+      <div v-if="getMonthData(current)" class="notes-month">
+        <section>{{ getMonthData(current) }}</section>
+        <span>Backlog number</span>
+      </div>
+    </template>
+  </a-calendar>
 </template>
-
 <script>
-export default {
-    name: 'Calendar',
-    props: {},
-    setup() {
-        
-    },
-}
-</script>
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+  setup() {
+    const value = ref();
 
+    const getListData = value => {
+      let listData;
+
+      switch (value.date()) {
+        case 8:
+          listData = [{
+            type: 'warning',
+            content: 'This is warning event.',
+          }, {
+            type: 'success',
+            content: 'This is usual event.',
+          }];
+          break;
+
+        case 10:
+          listData = [{
+            type: 'warning',
+            content: 'This is warning event.',
+          }, {
+            type: 'success',
+            content: 'This is usual event.',
+          }, {
+            type: 'error',
+            content: 'This is error event.',
+          }];
+          break;
+
+        case 15:
+          listData = [{
+            type: 'warning',
+            content: 'This is warning event',
+          }, {
+            type: 'success',
+            content: 'This is very long usual event。。....',
+          }, {
+            type: 'error',
+            content: 'This is error event 1.',
+          }, {
+            type: 'error',
+            content: 'This is error event 2.',
+          }, {
+            type: 'error',
+            content: 'This is error event 3.',
+          }, {
+            type: 'error',
+            content: 'This is error event 4.',
+          }];
+          break;
+
+        default:
+      }
+
+      return listData || [];
+    };
+
+    const getMonthData = value => {
+      if (value.month() === 8) {
+        return 1394;
+      }
+    };
+
+    return {
+      value,
+      getListData,
+      getMonthData,
+    };
+  },
+
+});
+</script>
 <style scoped>
-.cal-container{
-    width: 900px;
-    border-style: none;
+.events {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.events .ant-badge-status {
+  overflow: hidden;
+  white-space: nowrap;
+  width: 100%;
+  text-overflow: ellipsis;
+  font-size: 12px;
+}
+.notes-month {
+  text-align: center;
+  font-size: 28px;
+}
+.notes-month section {
+  font-size: 28px;
 }
 </style>
